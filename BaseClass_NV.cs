3bd9cbdabd56a1e875985df7a479c415e278914e
VadymNasypanyi <vadym.nasypanyi@gmail.com>
2021-09-11 16:42:15 +0300

    auth_commit

--- BaseClass_NV.cs
+++ BaseClass_NV.cs
@@ -20,7 +20,7 @@ namespace VadimFramework.Engine
 
         public String screenUrl = "https://github.com/";
         public String emailLogin = "nv.test1.demo@gmail.com"; 
-        public String passwordLogin = "u}4J=6gf3V2=s}";
+        public String passwordLogin = "CSWINksk2";
         public String RepositoryName = "Vadim_Repository_Final_Version_NV";
         public String RepositoryFullName = "VadimNas/Vadim_Repository_Final_Version_NV";
         public String Description = "Demo_Test_VNasypanyi_Creator_Description";
